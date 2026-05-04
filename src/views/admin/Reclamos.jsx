b8ef47b6d558a5e7a543aab7bfbd6a769e271fa9
Mauro Favre <mauro_favre@hotmail.com>
2026-05-03 23:53:16 -0300

    feat: formulario de reclamos y tabla de seguimiento terminada

--- src/views/admin/Reclamos.jsx
+++ src/views/admin/Reclamos.jsx
@@ -1,8 +1,25 @@
+import { ModalReclamos } from '../../components/layout/ModalReclamos.jsx'
+import { useState } from 'react';
+import { ListadoReclamos } from '../../components/layout/ListadoReclamos.jsx';
+
+
 export function Reclamos() {
+  //modal de reclamos
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>
+    <>
       <h1 className="h3 mb-3">Reclamos</h1>
       <p className="text-muted">Seguimiento de reclamos y mantenimiento.</p>
-    </div>
+     
+      <button className="btn btn-primary" onClick={handleShow}>
+        Nuevo reclamo
+        </button>
+       <ModalReclamos show={show} onHide={handleClose}/>
+       <hr />
+       <ListadoReclamos />
+    </>
   )
 }
